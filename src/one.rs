9c959ac645229865aaf067ab91c34ca998f78fec
Matero952 <mateofeit@gmail.com>
2025-04-25 23:51:58 -0600

    forgot a little logic thing in one

--- src/one.rs
+++ src/one.rs
@@ -1,8 +1,9 @@
-fn two_sum(nums: Vec<i32>, target: i32) -> Vec<usize>{
+// impl Solution {
+    fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32>{
     for (i_idx, num1) in nums.iter().enumerate() {
         for (j_idx, num2) in nums.iter().enumerate() {
-            if num1 + num2 == target {
-                let sol = vec![i_idx, j_idx];
+            if num1 + num2 == target && i_idx != j_idx {
+                let sol = vec![i_idx as i32, j_idx as i32];
                 return sol
             }
             else {
@@ -12,5 +13,4 @@ fn two_sum(nums: Vec<i32>, target: i32) -> Vec<usize>{
     } 
     return vec![]
 }
-        
-
+// }
